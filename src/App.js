868ef0bcc53d8e16bc37ee3af83d02180104f529
iquaresma <iquaresma@52north.org>
2023-04-18 10:14:39 +0200

    backup version befoe bus stops geojson

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
+import { busStops } from './data';
 
 import { MapContainer,TileLayer, Marker, Popup } from 'react-leaflet';
 import { Icon, divIcon } from 'leaflet';
@@ -9,21 +10,6 @@ import MarkerClusterGroup from 'react-leaflet-cluster';
 
 function App() {
   
-  const markers = [
-    {
-      geocode: [48.86, 2.3522],
-      popUp: "Hello, I am pop up 1"
-    },
-    {
-      geocode: [48.85, 2.3522],
-      popUp: "Hello, I am pop up 2"
-    },
-    {
-      geocode: [48.855, 2.34],
-      popUp: "Hello, I am pop up 3"
-    }
-  ];
-
   const customIcon = new Icon({
     //iconUrl: 'https://cdn-icons-png.flaticon.com/512/6153/6153497.png', import using cdn (url direct from flaticon)
     iconUrl: require('./assets/logo/marker-icon.png'),
@@ -39,18 +25,19 @@ function App() {
 
   }
 
+  //Generate Map
   return (
     <MapContainer center={[48.8566, 2.3522]} zoom={13}>
       <TileLayer 
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      
+
     <MarkerClusterGroup
       chunkedLoading //help with performance
       iconCreateFunction={createCustomClusterIcon}
     >
-      {markers.map(marker => (
+      {busStops.map(marker => (
         <Marker position={marker.geocode} icon={customIcon}>
           <Popup><h2>{marker.popUp}</h2></Popup>
         </Marker>
