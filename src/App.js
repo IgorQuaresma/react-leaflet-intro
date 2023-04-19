c953709b7bce5e571ff27e62ba1dcfcc979eb07c
iquaresma <iquaresma@52north.org>
2023-04-19 11:50:07 +0200

    plotting json data for freiburg, but taking long

--- src/App.js
+++ src/App.js
@@ -1,52 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import { busStops } from './data';
-import busStopsJson from './data/VAG.json';
+import busStopsData from "./data/VAG.json";
 
-import { MapContainer,TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
-import { Icon, divIcon } from 'leaflet';
-import MarkerClusterGroup from 'react-leaflet-cluster';
-import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+import { Icon, divIcon } from "leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+import "leaflet/dist/leaflet.css";
 
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import L from "leaflet";
 
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 function App() {
-  
-  const customIcon = new Icon({
-    //iconUrl: 'https://cdn-icons-png.flaticon.com/512/6153/6153497.png', import using cdn (url direct from flaticon)
-    iconUrl: require('./assets/logo/marker-icon.png'),
-    iconSize: [38, 38] //size of the icon - without that the siye will the the actual size of the image 
-  })
-
-  const createCustomClusterIcon = (cluster) => {
-    return new divIcon({
-      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
-      className: "custom-marker-cluster",
-      //iconSize: point(33, 33, true)
-    })
-
-  }
+  console.log(busStopsData);
 
   //Generate Map
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
-      <TileLayer 
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    <MapContainer center={[47.997791, 7.842609]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-    <MarkerClusterGroup
-      chunkedLoading //help with performance
-      iconCreateFunction={createCustomClusterIcon}
-    >
-      {busStops.map(marker => (
-        <Marker position={marker.geocode} icon={customIcon}>
-          <Popup><h2>{marker.popUp}</h2></Popup>
-          <GeoJSON data={busStopsJson} />
-        </Marker>
+      {busStopsData.map((station) => (
+        <Marker
+          key={station.properties.stop_name}
+          position={[
+            station.geometry.coordinates[1],
+            station.geometry.coordinates[0],
+          ]}
+        ></Marker>
       ))}
-    </MarkerClusterGroup>
     </MapContainer>
   );
 }
